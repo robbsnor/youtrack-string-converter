a60ad7f3be2a59f0f0c633acc28dcdabde2498bd
robbsnor1 <robbinschmidt@gmail.com>
2019-09-13 21:29:07 +0200

    cleansweep

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,2 @@
 
 import '../styles/style.scss';
-import 'fullpage.js/vendors/scrolloverflow.min.js';
-import fullpage from 'fullpage.js';
-
-// import 'https://connect.soundcloud.com/sdk/sdk-3.3.2.js';
-
-var fullPageInstance = new fullpage('#fullpage', {
-	// paddingTop: '45px',
-	menu: '.nav__main',
-	// paddingBottom: '145px',
-	// verticalCentered: false,
-	anchors:['label', 'music', 'events', 'djsets', 'merch']
-});
-
-
-console.log(Math.floor(Math.random() * 6) + 1  );
