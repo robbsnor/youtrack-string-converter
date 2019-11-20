8ec73dfee20b6a8228296cce6f06118a1d2989ae
Robbsnor <robbinschmidt@gmail.com>
2019-11-20 17:10:48 +0100

    sourcemaps

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,26 +4,28 @@ const common = require('./webpack.common.js');
 const ip = require('ip');
 
 module.exports = merge(common, {
-  mode: 'development',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-  	rules: [
-  		{
-  		  test: /\.(sa|sc|c)ss$/,
-  		  use: [
-  		    'style-loader',
-  		    'css-loader',
-  		    'sass-loader'
-  		  ]
-  		}
-  	]
-  },
-  devServer: {
-    host: ip.address(),
-    port: 8080,
-    contentBase: './dist'
-  },
+	mode: 'development',
+	module: {
+		rules: [{
+			test: /\.(sa|sc|c)ss$/,
+			use: [
+				'style-loader',
+				{
+					loader: 'css-loader',
+					options: {
+						sourceMap: true,
+					},
+				},
+				{
+					loader: 'sass-loader',
+					options: {
+						sourceMap: true,
+					},
+				},
+			],
+		}]
+	},
+	devServer: {
+		host: ip.address()
+	},
 });
