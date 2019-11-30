5af5711b6380d6bd7f86eeb99b193894ddae51dd
robbsnor1 <robbinschmidt@gmail.com>
2019-11-30 17:11:58 +0100

    repos

---
+++ src/js/dev/debug-pannel.js
@@ -0,0 +1,50 @@
+console.log('pannel')
+
+// Debug pannel
+function dPannelOpen() {
+  $('body').addClass('d_debug-pannel--open')
+  $('.d_debug-pannel').css({
+    'left': 0
+  })
+}
+
+function dPannelClose() {
+  $('body').removeClass('d_debug-pannel--open')
+  var OffsetLeft = $('.d_debug-pannel').width() - $('.d_debug-pannel-toggle').outerWidth();
+  $('.d_debug-pannel').css({
+    'left': -OffsetLeft
+  })
+}
+
+function dPannlToggle() {
+  if ($('body').hasClass('d_debug-pannel--open')) {
+    dPannelClose();
+  } else {
+    dPannelOpen();
+  }
+}
+
+
+// fire
+$('.d_debug-pannel-toggle').on('click', function () {
+  dPannlToggle();
+})
+
+
+
+// width and height indicator
+$(window).resize(function () {
+  var x = window.innerWidth;
+  var y = window.innerHeight;
+
+  $('.d_window-size-x .d_content').html(x)
+  $('.d_window-size-y .d_content').html(y)
+});
+
+
+
+// window scroll top indicator
+$(window).scroll(function () {
+  var wScroll = Math.round($(this).scrollTop());
+  $('.d_window-scroll-y .d_content').html(wScroll)
+});
