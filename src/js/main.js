9fcd9e45235a185628aee3364be9105b85768287
robbsnor1 <robbinschmidt@gmail.com>
2020-01-16 19:12:59 +0100

    readded icons

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,8 @@ window.$ = window.jQuery = jQuery;
 
 import "bootstrap";
 
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/brands'
 
 console.log('bobba')
