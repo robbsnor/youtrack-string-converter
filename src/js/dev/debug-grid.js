3d2bc9681aab05c1dbd695d8470f621be20ef133
Robbsnor <robbinschmidt@gmail.com>
2020-01-17 15:41:22 +0100

    dynamicly load html

--- src/js/dev/debug-grid.js
+++ src/js/dev/debug-grid.js
@@ -1,23 +1,27 @@
-// Debug grid overlay
-function dGridShow() {
-  $("body").addClass("d_debug-grid-overlay--show");
-  $(".d_debug-grid-overlay-toggle").addClass("active");
-}
+$(document).ready(function () {
 
-function dGridHide() {
-  $("body").removeClass("d_debug-grid-overlay--show");
-  $(".d_debug-grid-overlay-toggle").removeClass("active");
-}
-
-function dGridToggle() {
-  if ($("body").hasClass("d_debug-grid-overlay--show")) {
-    dGridHide();
-  } else {
-    dGridShow();
+  // Debug grid overlay
+  function dGridShow() {
+    $("body").addClass("d_debug-grid-overlay--show");
+    $(".d_debug-grid-overlay-toggle").addClass("active");
   }
-}
 
-// fire
-$(".d_debug-grid-overlay-toggle").on("click", function() {
-  dGridToggle();
+  function dGridHide() {
+    $("body").removeClass("d_debug-grid-overlay--show");
+    $(".d_debug-grid-overlay-toggle").removeClass("active");
+  }
+
+  function dGridToggle() {
+    if ($("body").hasClass("d_debug-grid-overlay--show")) {
+      dGridHide();
+    } else {
+      dGridShow();
+    }
+  }
+
+  // fire
+  $(".d_debug-grid-overlay-toggle").on("click", function () {
+    dGridToggle();
+  });
+  
 });
--- src/js/dev/debug-pannel.js
+++ src/js/dev/debug-pannel.js
@@ -1,66 +1,118 @@
-// debug pannel
-function dPannelOpen() {
-  $("body").addClass("d_debug-pannel--open");
-  $(".d_debug-pannel").css({
-    left: 0
-  });
-}
-
-function dPannelClose() {
-  $("body").removeClass("d_debug-pannel--open");
-  var OffsetLeft =
-    $(".d_debug-pannel").width() - $(".d_debug-pannel-toggle").outerWidth();
-  $(".d_debug-pannel").css({
-    left: -OffsetLeft
-  });
-}
-
-function dPannelToggle() {
-  if ($("body").hasClass("d_debug-pannel--open")) {
-    dPannelClose();
-  } else {
-    dPannelOpen();
-  }
-}
-
-
-
-
-// width and height indicator
-function setWHIndicators() {
-  var x = window.innerWidth;
-  var y = window.innerHeight;
-
-  $(".d_window-size-x .d_content").html(x);
-  $(".d_window-size-y .d_content").html(y);
-}
-
-
-
-// window scroll top indicator
-$(window).scroll(function() {
-  var wScroll = Math.round($(this).scrollTop());
-  $(".d_window-scroll-y .d_content").html(wScroll);
-});
-
-
-
-
-// fire debug pannel
-dPannelOpen();
-$(".d_debug-pannel-toggle").on("click", function () {
-  dPannelToggle();
-});
-
-// fire size indicators
-setWHIndicators();
-$(window).resize(function () {
-  setWHIndicators();
-});
-
-// close debug pannel on mobile
 $(document).ready(function () {
+
+  // debug pannel
+  function dPannelOpen() {
+    $("body").addClass("d_debug-pannel--open");
+    $(".d_debug-pannel").css({
+      left: 0
+    });
+  }
+
+  function dPannelClose() {
+    $("body").removeClass("d_debug-pannel--open");
+    var OffsetLeft =
+      $(".d_debug-pannel").width() - $(".d_debug-pannel-toggle").outerWidth();
+    $(".d_debug-pannel").css({
+      left: -OffsetLeft
+    });
+  }
+
+  function dPannelToggle() {
+    if ($("body").hasClass("d_debug-pannel--open")) {
+      dPannelClose();
+    } else {
+      dPannelOpen();
+    }
+  }
+
+
+
+
+  // width and height indicator
+  function setWHIndicators() {
+    var x = window.innerWidth;
+    var y = window.innerHeight;
+
+    $(".d_window-size-x .d_content").html(x);
+    $(".d_window-size-y .d_content").html(y);
+  }
+
+
+
+  // window scroll top indicator
+  $(window).scroll(function () {
+    var wScroll = Math.round($(this).scrollTop());
+    $(".d_window-scroll-y .d_content").html(wScroll);
+  });
+
+
+
+
+  // fire debug pannel
+  dPannelOpen();
+  $(".d_debug-pannel-toggle").on("click", function () {
+    dPannelToggle();
+  });
+
+  // fire size indicators
+  setWHIndicators();
+  $(window).resize(function () {
+    setWHIndicators();
+  });
+
+  // close debug pannel on mobile
   if (window.innerWidth < 500) {
     dPannelClose();
   }
+
+
 });
+
+
+
+// load frontend html
+$('body').append(`
+  <div class="d_debug-pannel" style="display: none">
+    <div class="item d_bootstrap-bp">
+        <div class="d_icon"><i class="fab fa-bootstrap"></i></div>
+        <div class="d_content"></div>
+    </div>
+    <div class="item d_window-size-x">
+        <div class="d_icon"><i class="fas fa-arrows-alt-h"></i></i></div>
+        <div class="d_content"></div>
+    </div>
+    <div class="item d_window-size-y">
+        <div class="d_icon"><i class="fas fa-arrows-alt-v"></i></div>
+        <div class="d_content"></div>
+    </div>
+    <div class="item d_window-scroll-y">
+        <div class="d_icon"><i class="fas fa-swimming-pool"></i></div>
+        <div class="d_content">-</div>
+    </div>
+    <div class="item d_debug-grid-overlay-toggle c-p">
+        <div class="d_icon m-0"><i class="fas fa-grip-lines-vertical"></i><i class="fas fa-grip-lines-vertical"></i></div>
+    </div>
+    <div class="item d_debug-pannel-toggle c-p">
+        <div class="d_icon m-0"><i class="fas fa-chevron-right"></i></div>
+    </div>
+    </div>
+
+    <div class="d_debug-grid-overlay" style="display: none">
+    <div class="container">
+        <div class="row">
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+            <div class="col"><div class="the-col"></div></div>
+        </div>
+    </div>
+  </div>
+`)
