f6a8f89fe57c201dfa723005686bdb00c42ccdc0
Robbsnor <robbinschmidt@gmail.com>
2020-08-20 03:30:10 +0200

    shared dev tasks + format

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,57 @@
 // gulp
-const gulp = require('gulp');
-const clean = require('gulp-clean');
-const mustache = require('gulp-mustache');
-const browserSync = require('browser-sync').create('bsServer');
+const gulp = require("gulp");
+const clean = require("gulp-clean");
+const mustache = require("gulp-mustache");
+const browserSync = require("browser-sync").create("bsServer");
 
 // vars
-const dev = require('./gulpfile.dev').functions;
-const prod = require('./gulpfile.prod').functions;
+const dev = require("./gulpfile.dev").functions;
+const prod = require("./gulpfile.prod").functions;
 
-const justMoveFileTypes = 'png,jpg,gif,svg,php,pdf';
-
-
+const justMoveFileTypes = "png,jpg,gif,svg,php,pdf";
 
 // shared tasks
-function compileTemplates () {
-  return gulp.src('./src/**/*.html')
-    .pipe(mustache())
-    .pipe(gulp.dest('./dist/'))
+function compileTemplates() {
+  return gulp.src("./src/**/*.html").pipe(mustache()).pipe(gulp.dest("./dist/")).pipe(browserSync.stream());
+}
+
+function moveFiles() {
+  return gulp
+    .src("./src/**/*.{" + justMoveFileTypes + "}")
+    .pipe(gulp.dest("./dist/"))
     .pipe(browserSync.stream());
 }
 
-function moveFiles () {
-  return gulp.src('./src/**/*.{' + justMoveFileTypes + '}')
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.stream());
+function deleteDist() {
+  return gulp.src("./dist", { read: false, allowEmpty: true }).pipe(clean());
 }
-
-function deleteDist () {
-  return gulp.src('./dist', {read: false, allowEmpty: true}).pipe(clean());
-}
-
-
 
 // browsersync
-function bsServe () {
+function bsServe() {
   browserSync.init({
     server: {
-      baseDir: './dist',
-      index: '/index.html'
+      baseDir: "./dist",
+      index: "/index.html",
     },
-    open: 'external',
+    open: "external",
     notify: false,
-    ghostMode: false
+    ghostMode: false,
   });
 
-  gulp.watch('./src/**/*.{' + justMoveFileTypes + '}', moveFiles);
-  gulp.watch('./src/**/*.mustache', compileTemplates);
-  gulp.watch('./src/**/*.html', compileTemplates);
-  gulp.watch('./src/**/*.scss', dev.compileScss);
-  gulp.watch('./src/**/*.js', dev.compileWebpack);
-  gulp.watch('./src/**/*.json', dev.compileWebpack);
+  gulp.watch("./src/**/*.{" + justMoveFileTypes + "}", moveFiles);
+  gulp.watch("./src/**/*.mustache", compileTemplates);
+  gulp.watch("./src/**/*.html", compileTemplates);
+  gulp.watch("./src/**/*.scss", dev.compileScss);
+  gulp.watch("./src/**/*.js", dev.compileWebpack);
+  gulp.watch("./src/**/*.json", dev.compileWebpack);
 }
 
-
-
 // register tasks
-exports.start     = gulp.series(deleteDist, gulp.parallel(compileTemplates, dev.compileScss, dev.compileWebpack, moveFiles), bsServe);
-exports.compile   = gulp.series(deleteDist, gulp.parallel(compileTemplates, dev.compileScss, dev.compileWebpack, moveFiles));
+// dev
+const devCompile = gulp.series(deleteDist, gulp.parallel(compileTemplates, dev.compileScss, dev.compileWebpack, moveFiles));
 
+exports.start = gulp.series(devCompile, bsServe);
+exports.compile = devCompile;
+
+// prod
 exports.prod = gulp.series(deleteDist, gulp.parallel(compileTemplates, prod.compileScss, prod.compileWebpack, moveFiles));
