138b00bb226a862cd0e67834e7d592776fce2acb
Robbsnor <robbinschmidt@gmail.com>
2021-02-09 14:36:57 +0100

    fed only on dev

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,12 @@
-require('front-end-debug');
+// dev
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    require("front-end-debug");
+}
+
+////
+///
+//
+
 
 require('./smoothAnchorScroll')
