104c821f0f2fad7255cb704b44506181937a12af
robbsnor <robbinschmidt@gmail.com>
2021-12-14 17:42:07 +0100

    ang

---
+++ src/app/components/converter/converter.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.scss']
+})
+export class ConverterComponent implements OnInit {
+    issue = new FormControl('');
+    formatedIssue = '';
+
+    constructor() {
+        this.convert();
+    }
+
+    ngOnInit(): void {
+        this.convert();
+    }
+
+    convert(): void {
+        this.issue.valueChanges.subscribe((val: string) => {
+
+            let newVal = val.replace(/\s+/g, '-');
+            newVal = newVal.replace(/["'“”();:]/g, "")
+            this.formatedIssue = newVal;
+        });
+    }
+}
