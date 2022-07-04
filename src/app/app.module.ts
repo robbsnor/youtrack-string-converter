c8ccaa633d0dd7f8ef715c57c414ad66c56b5042
unknown <robbinschmidt@gmail.com>
2022-07-04 22:57:38 +0200

    feat: new multiple choise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ConverterComponent } from './components/converter/converter.component';
-import { CopyHintComponent } from './components/copy-hint/copy-hint.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConverterComponent,
-    CopyHintComponent,
     FooterComponent
   ],
   imports: [
--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { FormControl, FormGroup } from '@angular/forms';
 
@@ -12,43 +12,48 @@ interface Issue {
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.scss']
 })
-export class ConverterComponent implements OnInit {
-    issue = new FormGroup({
+export class ConverterComponent {
+    public issue = new FormGroup({
         prefix: new FormControl('feature'),
         title: new FormControl('')
     });
 
-    branchName = '';
-    copyComplete = false;
+    public copyVariations: string[] = [];
+
+    public branchName = '';
+
+    constructor(private clipboard: Clipboard) {
+        this.issue.valueChanges.subscribe((issue: Issue) => {
+            this.branchName = this.getBranchGame(issue);
+
+            this.copyVariations = [
+                this.branchName,
+                `git checkout -b ${this.branchName}`,
+                `git checkout master && git pull && git checkout -b ${this.branchName}`
+            ]
+        });
+    }
+
+    getBranchGame(issue: Issue) {
+        // remove unwanted characters
+        const title = issue.title.replace(/\s+/g, '-').replace(/["'“”();:!?.,\[\]]/g, "");
+
+        // casing
+        const projectName = title.substring(0, 3).toUpperCase();
+        const restOfTitle = title.substring(3).toLowerCase();
+
+        const prefix = (issue.prefix === 'none') ? '': `${issue.prefix}/`;
+
+        const branchName = prefix + projectName + restOfTitle;
+
+        return branchName
+    }
 
     capitalizeFirstLetter(string: string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    constructor(private clipboard: Clipboard) {
-
-        this.issue.valueChanges.subscribe((issue: Issue) => {
-            // remove unwanted characters
-            let title = issue.title.replace(/\s+/g, '-').replace(/["'“”();:!?.,\[\]]/g, "");
-
-            // casing
-            let projectName = title.substring(0, 3).toUpperCase();
-            let restOfTitle = title.substring(3).toLowerCase();
-
-            let prefix = (issue.prefix === 'none') ? '': `${issue.prefix}/`;
-
-            let branchName = prefix + projectName + restOfTitle;
-            this.branchName = branchName;
-
-            console.log(this.branchName);
-        });
-    }
-
-    ngOnInit(): void {
-    }
-
-    copy(): void {
-        this.clipboard.copy(this.branchName);
-        this.copyComplete = true;
+    copy(text: string): void {
+        this.clipboard.copy(text);
     }
 }
